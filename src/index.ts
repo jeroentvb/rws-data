585b1dab601e791897b7deea9f280f1280b3aa1e
jeroentvb <36192730+jeroentvb@users.noreply.github.com>
2022-04-22 21:01:51 +0200

    refactor: exports

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import { getLocations } from './modules/get-locations.js';
-import { getMetadata } from './modules/get-metadata.js';
-import { getObservations } from './modules/get-observations.js';
-import { getLatestObservations } from './modules/get-latest-observations.js';
+export { getLocations } from './modules/get-locations.js';
+export { getMetadata } from './modules/get-metadata.js';
+export { getObservations } from './modules/get-observations.js';
+export { getLatestObservations } from './modules/get-latest-observations.js';
 
-/**
- * Todo export interfaces
- */
-
-export default {
-   getLocations,
-   getMetadata,
-   getObservations,
-   getLatestObservations,
-};
+export * from './interfaces';
